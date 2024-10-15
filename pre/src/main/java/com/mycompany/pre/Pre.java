0ede1620dc97e5fd6a293ac75871b7a34873e940
AdamThecooders <wealthadam23@gmail.com>
2024-10-15 08:27:41 +0800

    di ko alam

---
+++ pre/src/main/java/com/mycompany/pre/Pre.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pre;
+
+/**
+ *
+ * @author user
+ */
+import java.util.*;
+public class Pre {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
